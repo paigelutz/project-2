bcd28c452cc3898542e35000ced39297afcbd293
Paige Lutz <plutz@uoregon.edu>
2020-04-27 17:06:53 -0700

    extra credited added

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,20 +1,4 @@
 // jshint esversion: 6
-
-let containsDuplicates = s => {
-  let cArr = [0, 0, 0, 0],
-    code;
-  for (let i in cArr) {
-    code = s[i].toUpperCase().charCodeAt(0) - 65;
-    //console.log(code);
-    if (cArr[code] > 0) {
-      return true;
-    } else {
-      cArr[i]++;
-    }
-  }
-  return false;
-};
-
 //revWords 1 (for loop)
 
 let revWord = (word) => {
@@ -76,4 +60,20 @@ let revWords3 = (sent) => {
     result = result + revWord(words3) + " ";
   }
   return result;
+};
+
+let containsDuplicates = (s) => {
+  let cCou = new Array(26).fill(0);
+  let cArr = Array.from(s.toUpperCase());
+  for (let i in cArr) {
+    if (cArr[i] != " "){
+      let index = s.toUpperCase().charCodeAt(i) - 65;
+      if (cCou[index] == 0) {
+        cCou[index] = cArr[i];
+      } else {
+        return true;
+      }
+    }
+  }
+  return false;
 };
